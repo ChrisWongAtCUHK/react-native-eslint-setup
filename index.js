c1584d2c285283b492b4a9625d0f3698a5f0a32d
Chris Wong <wc38.wong@gmail.com>
2020-02-25 21:48:47 +0800

    Read package.json

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
 // eslint-disable-next-line no-console
-const log = console.log
+const log = console.log;
+const fs = require('fs');
 
+fs.readFile('package.json', (err, data) => {
+	if (err) {
+		log(err);
+	}
+  let pkg = JSON.parse(data);
+
+	log(pkg);
+});
